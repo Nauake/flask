eec89e7a2c155c1b3be1ebf4ab07e8a8cf291e70
Nauake <kauanauhelbiton@gmail.com>
2025-07-08 15:15:48 -0300

    atualizando

--- classificacao/__init__.py
+++ classificacao/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask
 
 app = Flask(__name__)
 
-from classificacao.routes import homepage
+from classificacao.routes import home
--- classificacao/routes.py
+++ classificacao/routes.py
@@ -1,7 +1,15 @@
 from classificacao import app
-from flask import render_template
+from flask import render_template, url_for, request
+import pandas as pd
+
+
 
 @app.route("/")
-def homepage():
-    return render_template("index.html")
+def home():
+    path = r"C:\Users\Auhelbiton\OneDrive\Documentos\Classificacao_Infa.xlsx"
+    dados = pd.read_excel(path, skiprows=1, usecols=range(8))
+    nomes = dados['NOME'].tolist()
+    nomes.insert(0, "TODOS OS NOMES")
+
+    return render_template('index.html', nomes=nomes, dados=dados)
 
